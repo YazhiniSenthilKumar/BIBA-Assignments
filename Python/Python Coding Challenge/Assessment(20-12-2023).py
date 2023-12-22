4870d51239fc437730437f3bd12900116aeb362d
Yazhini S <88731614+YazhiniSenthilKumar@users.noreply.github.com>
2023-12-22 11:46:31 +0530

    Add files via upload

---
+++ Python/Python Coding Challenge/Assessment(20-12-2023).py
@@ -0,0 +1,35 @@
+#1.Explain Python Module with examples
+#a.Import module in Python .Renaming the Python module
+
+import pythonmoduleass as ps
+
+Addition = ps.add(5,3)
+Subtract = ps.sub(30,20)
+Multiply = ps.multi(9,8)
+Divide = ps.div(6,2)
+
+print("ADD RESULT : ",Addition)
+print("SUB RESULT : ",Subtract)
+print("MULTI RESULT : ",Multiply)
+print("DIVIDE RESULT : ", Divide)
+
+
+
+
+
+#Explain pandas and numpy using examples in python
+
+#Using python
+
+import pandas as ps
+data=ps.read_csv("Salary_Data.csv")
+print(data)
+
+
+#Using numpy
+
+import numpy as np
+
+a=np.array([3,4,5,6,7])
+b= a*2
+print(b)'''
